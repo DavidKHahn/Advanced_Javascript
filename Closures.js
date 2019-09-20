653c66d9d833efc5612108f66405606e6f2c6495
David <dhahn123@gmail.com>
2019-09-19 21:34:49 -0700

    Closures

---
+++ Closures.js
@@ -0,0 +1,108 @@
+// CLOSURES: functions + lexical scope
+
+function a() {
+    let grandpa = 'grandpa'
+    return function b() {
+        let father = 'father'
+        return function c() {
+            let son = 'son'
+            return `${grandpa} > ${father} > ${son}`
+        }
+    }
+}
+
+console.log(a()()())
+
+const boo = (string) => (name) => (name2) => console.log(`${string} ${name} ${name2}`)
+
+const booString = boo('hi');
+
+// 5 years
+const booStringName = booString()
+
+// CLOSURES
+
+Memory efficient
+function heavyDuty(index) {
+    const bigArray = new Array(7000).fill('Hello')
+    console.log('created!')
+    return bigArray[index]
+}
+
+console.log(heavyDuty(699));
+console.log(heavyDuty(699));
+console.log(heavyDuty(699));
+
+const getHeavyDuty = heavyDuty2();
+getHeavyDuty(544);
+getHeavyDuty(222);
+getHeavyDuty(534);
+
+
+function heavyDuty2() {
+    const bigArray = new Array(7000).fill('Hello')
+    console.log('created Again!')
+    return function(index) {
+        return bigArray[index]
+    }
+}
+// Encapsulation
+const makeNuclearButton = () => {
+    let timeWithouDestruction = 0;
+    const passTime = () => timeWithouDestruction++;
+    const totalPeaceTime = () => timeWithouDestruction;
+    const launch = () => {
+        timeWithouDestruction = -1;
+        return 'BOOM';
+    }
+    setInterval(passTime, 1000)
+    return {
+        // launch,
+        totalPeaceTime
+        }
+    }
+
+    const ohno = makeNuclearButton();
+    ohno.totalPeaceTime()
+
+let view;
+function initialize() {
+    let called = 0;
+    return function() {
+        if (called > 0) {
+            return;
+        } else {
+            view = 'MOUNTAIN';
+            called++;
+            console.log('view has been set!')
+        }
+    }
+
+}
+
+const startOnce = initialize();
+startOnce();
+startOnce();
+startOnce();
+console.log(view)
+
+const array = [1,2,3,4];
+for (let i =0; i<array.length; i++){
+    setTimeout(function() {
+        console.log('I am at index ' + array[i])
+    }, 3000)
+}
+
+// I am at index 1
+// I am at index 2
+// I am at index 3
+// I am at index 4
+
+const array = [1,2,3,4];
+
+for (var i = 0; i < array.length; i++){
+    (function(closureI){
+        setTimeout(function() {
+        console.log('I am at index ' + array[closureI])
+    }, 3000)})(i)
+}
