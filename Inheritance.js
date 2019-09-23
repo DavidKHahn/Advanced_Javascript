77403ffd4f211df13f390309db16ae94cf1d121b
David <dhahn123@gmail.com>
2019-09-22 20:29:15 -0700

    four pillars of OOP: polymorphism, inheritance, abstraction, encapsulation

--- Inheritance.js
+++ Inheritance.js
@@ -1,5 +1,6 @@
 // ES6 Class
 class Character {
+    // #age = 54; // private data used for only this class
     constructor(name, weapon) {
         this.name = name;
         this.weapon = weapon;
@@ -16,13 +17,20 @@ class Elf extends Character {
         console.log(this) // 'this' refers to the super class: 'Character' and it's properties
         this.type = type
     }
+    // basic example of Polymorphism: method overriding for each 'class'
+    attack(cry){
+        return "attack with " + cry
+    }
 }
-
 
 class Ogre extends Character {
     constructor(name, weapon, color) {
         super(name, weapon);
         this.color = color;
+    }
+    // basic example of Polymorphism
+    attack() {
+        return "argghhhhhhhhh!"
     }
     makeFort() {
         return 'Strongest fort in the world made!'
@@ -31,8 +39,20 @@ class Ogre extends Character {
 
 const dolby = new Elf('Dolby', 'cloth', 'house');
 console.log(dolby)
-console.log(dolby.attack());
+console.log(dolby.attack('weee')); // basic example of Polymorphism
 
 const shrek = new Ogre('Shrek', 'club', 'green');
-console.log(shrek)
+console.log(shrek.attack()) // basic example of Polymorphism
 console.log(shrek.makeFort())
+// console.log(Ogre.prototype.isPrototypeOf(shrek)) // '.prototype' is needed to target the object
+// console.log(Character.prototype.isPrototypeOf(Ogre.prototype))
+console.log(dolby instanceof Elf) // simpler syntax use for checking prototypes
+console.log(dolby instanceof Character)
+// at the end of the day with Javascript we have objects inheriting from other objects
+
+// FOUR PILLARS OF OBJECT ORIENTED PROGRAMMING:
+
+// POLYMORPHISM - customizing methods within each class
+// INHERITANCE - avoid having to rewrite code, saves memory space (shared memory space, inheriting from other classes)
+// ABSTRACTION - hides complexity from user (i.e. creating simpler interfaces, here are methods & properties but works under the hood)
+// ENCAPSULATION - organizes into units (i.e. wrapping code into boxes, easier to maintain and reuse)
