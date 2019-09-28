bed548c5becde29e4341be83a81c352cdaf1a957
David <dhahn123@gmail.com>
2019-09-28 15:32:26 -0700

    IIFE module pattern: reveal

--- Modules/script.js
+++ Modules/script.js
@@ -1,11 +1,24 @@
-var harry = "potter"
-var voldemort = "He who must not be named"
+// Global Scope
+    // Module Scope
+        // Function Scope
+            // Block Scope - let and const
 
-function fight(char1, char2) {
-    var attack1 = Math.floor(Math.random() * char1.length);
-    var attack2 = Math.floor(Math.random() * char2.length);
-    console.log(attack1)
-    return attack1 > attack2 ? `${char1} wins`: `${char2} wins`
-}
+// IIFE: automatically runs on console of browser page
+// Module Pattern - reveal module pattern(example below)
+var fightModule = (function (){
+    var harry = "potter"
+    var voldemort = "He who must not be named"
 
-fight(harry, voldemort)
+    function fight(char1, char2) {
+        var attack1 = Math.floor(Math.random() * char1.length);
+        var attack2 = Math.floor(Math.random() * char2.length);
+        console.log(attack1)
+        return attack1 > attack2 ? `${char1} wins`: `${char2} wins`
+    }
+    return {
+        fight
+    }
+})()
+
+
+
